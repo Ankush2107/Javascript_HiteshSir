9f9db48c65ab8bc1cff4f4d60ef1d0862edefc2b
Ankush2107 <ankush.kumar210701@gmail.com>
2023-07-16 16:42:10 +0530

    First class of DOM

--- scopes.js
+++ scopes.js
@@ -1,37 +1,36 @@
-// // var b = 300;
-// let a = 300;
+// // // var b = 300;
+// // let a = 300;
 
-// if(true){ 
-//     let a = 10;
-//     // var b = 20;
-//     const c = 30;
+// // if(true){ 
+// //     let a = 10;
+// //     // var b = 20;
+// //     const c = 30;
+// // }
+
+// // // console.log(a);
+// // console.log(a);
+// // // console.log(c);
+
+// function one() {
+//     const username = "Ankush";
+
+//     function two() {
+//         const website = "youtube";
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     // two();
+// }
+// one();
+
+// // Mini Hoisting
+// console.log(addOne(10));
+// function addOne(num) {
+//     return num + 1;
 // }
 
-// // console.log(a);
-// console.log(a);
-// // console.log(c);
-
-function one() {
-    const username = "Ankush";
-
-    function two() {
-        const website = "youtube";
-        console.log(username);
-    }
-    // console.log(website);
-    // two();
-}
-one();
-
-// Mini Hoisting
-console.log(addOne(10));
-function addOne(num) {
-    return num + 1;
-}
-
+// // console.log(addTwo(10));
+// const addTwo = function(num) {
+//     return num + 2;
+// }
 // console.log(addTwo(10));
-const addTwo = function(num) {
-    return num + 2;
-}
-console.log(addTwo(10));
-
