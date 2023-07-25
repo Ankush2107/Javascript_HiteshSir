7d1d49a31feb4351e993ef6f5e3672024ea00231
Ankush2107 <ankush.kumar210701@gmail.com>
2023-07-25 11:03:23 +0530

    I have learned then and catch

--- promises/lecture1.js
+++ promises/lecture1.js
@@ -21,3 +21,64 @@ new Promise((resolve, reject) => {
 }).then(function() {
     console.log("Asyn 2 resolved");
 })
+
+
+
+const promiseThree = new Promise((resolve, reject) => {
+
+    setTimeout(function() {
+        resolve({username: "Ankush", email: "ankush.kumar210701@gmail.com"})
+    } ,1000)
+})
+
+promiseThree.then(function(user) {
+    console.log(user);
+})
+
+
+const promiseFour = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        let error = false
+        if(!error) {
+            resolve({username: "Ankush", password: "kumar2107"});
+        }else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username;
+}).then((username) => {
+    console.log(username);
+}).catch((error) => {
+    console.log(error);
+}).finally(() => {
+    console.log("The promise is either resolved or rejected");
+})
+
+
+const promiseFive = new Promise((resolve, reject) => {
+
+    setTimeout(function() {
+        let error = true
+        if(!error) {
+            resolve({username: "Javascript", password: "123"});
+        }else {
+            reject('ERROR: Js went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive() {
+    try {
+        const response = await promiseFive;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+consumePromiseFive()
